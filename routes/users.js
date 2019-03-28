6efe26437771baa6693ac0351b16c5babe269e7b
ParaSLee <610033941@qq.com>
2019-03-28 23:41:54 +0800

    添加管理员登录接口

--- routes/users.js
+++ routes/users.js
@@ -271,4 +271,39 @@ router.get("/getUser", async (ctx, next) => {
     };
   }
 });
+// 管理员登录
+router.post("/Mlogin", async (ctx, next) => {
+  const { user, password } = ctx.request.body;
+  let DB_user = await User.findOne({ user });
+  if (DB_user) {
+    if (DB_user.password === password) {
+      ctx.body = {
+        code: 0,
+        msg: "",
+        data: {
+          code: 0,
+          msg: "success"
+        }
+      };
+    } else {
+      ctx.body = {
+        code: 0,
+        msg: "",
+        data: {
+          code: -1,
+          msg: "用户名或者密码错误"
+        }
+      };
+    }
+  } else {
+    ctx.body = {
+      code: 0,
+      msg: "",
+      data: {
+        code: -1,
+        msg: "用户名或者密码错误"
+      }
+    };
+  }
+})
 module.exports = router;
